4f5ef6abb18d5bf8429282c5bebed4d9ac9f07e9
Naresh Bhosale <nareshbhosale73@gmail.com>
2024-12-20 05:21:44 +0530

    Refactor industry_type validation and clean up console logs in Step1 component

--- src/pages/AddOrganisation/components/step-1.jsx
+++ src/pages/AddOrganisation/components/step-1.jsx
@@ -1,6 +1,3 @@
-
-
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Business,
@@ -34,33 +31,36 @@ const organizationSchema = z.object({
     { message: "Foundation date must be less than or equal to current date" }
   ),
   web_url: z.string().optional(),
-  industry_type: z.string().optional().refine(
-    (val) => {
-      const predefinedValues = [ 
-        "Technology",
-        "Finance",
-        "Healthcare",
-        "Education",
-        "Manufacturing",
-        "Retail",
-        "Transportation",
-        "Telecommunications",
-        "Real Estate",
-        "Hospitality",
-        "Pharmaceuticals",
-        "Automotive",
-        "Insurance",
-        "Nonprofit",
-        "Government",
-        "Consulting",
-        "Media",
-        "Advertising",
-        "Biotechnology",
-      ];
-      return predefinedValues.includes(val) || val === "other";
-    },
-    { message: "Invalid industry type" }
-  ),
+  industry_type: z
+    .string()
+    .optional()
+    .refine(
+      (val) => {
+        const predefinedValues = [
+          "Technology",
+          "Finance",
+          "Healthcare",
+          "Education",
+          "Manufacturing",
+          "Retail",
+          "Transportation",
+          "Telecommunications",
+          "Real Estate",
+          "Hospitality",
+          "Pharmaceuticals",
+          "Automotive",
+          "Insurance",
+          "Nonprofit",
+          "Government",
+          "Consulting",
+          "Media",
+          "Advertising",
+          "Biotechnology",
+        ];
+        return predefinedValues.includes(val) || val === "other";
+      },
+      { message: "Invalid industry type" }
+    ),
   custom_industry_type: z.string(),
   email: z.string().email(),
   organization_linkedin_url: z.string().optional(),
@@ -86,7 +86,7 @@ const organizationSchema = z.object({
 const Step1 = ({ nextStep }) => {
   // to state, hook , import other funciton
   const { decodedToken } = useGetUser();
-  const { 
+  const {
     orgName,
     foundation_date,
     web_url,
@@ -122,7 +122,10 @@ const Step1 = ({ nextStep }) => {
   });
 
   const { errors } = formState;
-console.log("gst_number",gst_number);
+  console.log(`🚀 ~ file: step-1.jsx:125 ~ errors:`, errors);
+  console.log(`🚀 ~ file: step-1.jsx:106 ~ watch:`, watch());
+
+  console.log("gst_number", gst_number);
 
   const onSubmit = async (data) => {
     if (data.industry_type === "other") {
@@ -183,7 +186,7 @@ console.log("gst_number",gst_number);
             errors={errors}
             error={errors.contact_number}
           />
-         
+
           <AuthInputFiled
             name="industry_type"
             icon={FactoryOutlined}
@@ -215,7 +218,7 @@ console.log("gst_number",gst_number);
               { value: "Biotechnology", label: "Biotechnology" },
               { value: "other", label: "Other" },
             ]}
-          /> 
+          />
           {/*  */}
           {watch("industry_type") === "other" && (
             <AuthInputFiled
@@ -229,9 +232,7 @@ console.log("gst_number",gst_number);
               error={errors.custom_industry_type}
             />
           )}
-          
-          
-         
+
           <AuthInputFiled
             className="w-full"
             name="location"
@@ -244,7 +245,7 @@ console.log("gst_number",gst_number);
             error={errors.location}
             value={watch("location")}
           />
-           <AuthInputFiled
+          <AuthInputFiled
             name="web_url"
             icon={Link}
             control={control}
@@ -264,7 +265,7 @@ console.log("gst_number",gst_number);
             errors={errors}
             error={errors.organization_linkedin_url}
           />
-           <AuthInputFiled
+          <AuthInputFiled
             name="description"
             icon={Description}
             control={control}
